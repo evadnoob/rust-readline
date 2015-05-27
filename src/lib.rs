c813f73ce667ecf24b9da2161d1bca1d11d362f5
David Boon <daveb@porch.com>
2015-05-27 06:33:42 -0700

    modified a little to make result capture the actual readline error.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-#![crate_type = "lib"]
-extern crate libc;
 
-use std::c_str;
+use std::ffi::{CStr, CString};
+use std::str;
 
 mod ext_readline {
-    use libc::c_char;
+    extern crate libc;
+    use self::libc::c_char;
     #[link(name = "readline")]
     extern {
         pub fn add_history(line: *const c_char);
@@ -14,19 +14,23 @@ mod ext_readline {
 
 pub fn add_history(line: &str) {
     unsafe {
-        ext_readline::add_history(line.to_c_str().as_ptr());
+        ext_readline::add_history(CString::new(line).unwrap().as_ptr());
     }
 }
 
-pub fn readline(prompt: &str) -> Option<String> {
-    let cprmt = prompt.to_c_str();
+
+pub fn readline(prompt: &str) -> Result<String, String> {
+    let cprmt = CString::new(prompt).unwrap();
     unsafe {
-        let ret = ext_readline::readline(cprmt.as_ptr());
-        if ret.is_null() {  // user pressed Ctrl-D
-            None
-        }
-        else {
-            c_str::CString::new(ret, true).as_str().map(|ret| ret.to_string())
+        let ptr = ext_readline::readline(cprmt.as_ptr());
+        if ptr.is_null() {  // user pressed Ctrl-D
+            return Err("null pointer".to_string());
+        } else {
+            let ret = str::from_utf8(CStr::from_ptr(ptr).to_bytes());
+            let ret = ret.ok().map(|s| s.to_string());
+            ext_readline::libc::free(ptr as *mut _);
+            
+            return Ok(ret.unwrap());
         }
     }
 }
